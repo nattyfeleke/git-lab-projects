fbe947b7c3b3ae532025bce69c79f5f4fffd1f49
samueladdisu <samueladdisu7@gmail.com>
2023-03-28 05:40:29 +0300

    first commmit and set up the routes and components for the project

---
+++ src/components/BreadCrumb.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+
+import { ChevronRightIcon } from "@heroicons/vue/20/solid";
+</script>
+
+<template>
+  <div class="flex items-center gap-x-1 border-b border-gray-400 py-3">
+    <a href="#"> <h1 class="text-sm leading-6 text-gray-400">Explore</h1></a>
+
+    <ChevronRightIcon
+      class="h-5 w-5 flex-none text-gray-400"
+      aria-hidden="true"
+    />
+    <RouterLink to="/projects">
+      <h1 class="text-sm leading-6 font-semibold text-gray-900">
+        Projects
+      </h1></RouterLink
+    >
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Project from "../components/Project.vue";
+</script>
+<template>
+  <h1 class="text-gray-800 font-semibold py-3 text-2xl">Explore Projects</h1>
+  <div class="bg-white">
+    <nav class="flex flex-col sm:flex-row border-b">
+      <a
+        href="#"
+        class="text-gray-600 py-3 px-4 block hover:text-blue-500 text-gray-800 border-b-2 font-medium border-blue-500"
+      >
+        All</a
+      ><a
+        href="#"
+        class="text-gray-600 py-3 px-4 block hover:border-b-2 hover:border-blue-200"
+      >
+        Most starred</a
+      ><a
+        href="#"
+        class="text-gray-600 py-3 px-4 block hover:border-b-2 hover:border-blue-200"
+      >
+        Trending</a
+      >
+    </nav>
+  </div>
+  <Project />
+</template>
+
+
+<style lang="scss" scoped>
+</style>
